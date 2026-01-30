1c052af28bebdeeb9762e25a3bdecc8b03e9a128
blackbot00 <109160013+blackbot00@users.noreply.github.com>
2026-01-30 21:06:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import BOT_TOKEN
 from app.database.mongo import init_db
-from app.handlers import start, common, registration, chat_ai, chat_human, premium
+from app.handlers import start, common, registration, chat_ai, chat_human, premium, profile
 from app.admin import commands
 
 logging.basicConfig(level=logging.INFO)
@@ -17,6 +17,7 @@ chat_ai.register(dp)
 chat_human.register(dp)
 premium.register(dp)
 common.register(dp)
+profile.register(dp)
 
 # Admin handlers
 commands.register(dp)
